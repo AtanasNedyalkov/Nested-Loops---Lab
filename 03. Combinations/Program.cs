06a2dafee0e9ae05a39844c5e4e0abf5e3ea2667
Admin <Admin@DESKTOP-QFVKQ9F>
2022-06-11 12:00:02 +0300

    Add project files.

---
+++ 03. Combinations/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _03._Combinations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            int counter = 0;
+            for (int a = 0; a <= input; a++)
+            {
+                for (int b = 0; b <= input; b++)
+                {
+                    for (int c = 0; c <= input; c++)
+                    {
+                        int sum = a + b + c;
+                        if (sum == input)
+                        {
+                            counter++;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
---
+++ 04. Sum of Two Numbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _04._Sum_of_Two_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startInterval = int.Parse(Console.ReadLine());
+            int endInterval = int.Parse(Console.ReadLine());
+            int magicNum = int.Parse(Console.ReadLine());
+            int combinationSum = 0;
+            int combinationNum = 0;
+            for (int a = startInterval; a <=endInterval ; a++)
+            {
+                for (int b = startInterval; b <= endInterval; b++)
+                {
+                    combinationSum = a + b;
+                    combinationNum++;
+                    if (combinationSum == magicNum)
+                    {
+                        Console.WriteLine($"Combination N:{combinationNum} ({a} + {b} = {magicNum})");
+                        return;
+                    }
+                }
+            }
+           Console.WriteLine($"{combinationNum} combinations - neither equals {magicNum}"); 
+        }
+    }
+}
